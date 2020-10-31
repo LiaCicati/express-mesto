f784c08a063209505b825fe40dff14a050c0bc59
Lia Cicati <LiaCicati@yandex.com>
2020-10-31 19:59:59 +0100

    fixed: directory and imports

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
-
-const app = express();
-const { PORT = 3000 } = process.env;
 const path = require('path');
 const usersRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards.js');
 
+const app = express();
+const { PORT = 3000 } = process.env;
+
 app.use(express.static(path.join(__dirname, './public')));
 
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
